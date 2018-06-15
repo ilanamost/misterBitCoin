b357c1ee9c1c6f8e0d1e1579b0fbc12eae69ae71
Ilana Mostovski <ilana.mostovski@gmail.com>
2018-06-15 17:38:52 +0300

    Updates

--- src/store/types.js
+++
@@ -1,5 +0,0 @@
-export const ADD_CONTACT = "ADD_CONTACT";
-export const LOAD_CONTACTS = "LOAD_CONTACTS";
-export const GET_CONTACT_BY_ID = "GET_CONTACT_BY_ID";
-export const LOAD_USER = "LOAD_USER";
-export const UPDATE_USER = "UPDATE_USER";
